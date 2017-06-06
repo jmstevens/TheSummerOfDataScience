fa610ffe029b90ad584d4cf019e097c6218c06ac
Josh Rzemien <jrzemien@adaptiverx.com>
2017-06-06 09:23:39 -0500

    this is a .py file that should pull data from google stock prices and predict the next 33 days of prices past the data. it also graphs this prediction and prints the accuracy. it may not be 100% functional but it runs. there are several dependencies listed at the top

---
+++ regression intro.py
@@ -0,0 +1,69 @@
+import pandas as pd
+import quandl, math, datetime
+import numpy as np
+from sklearn import preprocessing, cross_validation, svm
+from sklearn.linear_model import LinearRegression
+import matplotlib.pyplot as plt
+from matplotlib import style
+
+style.use('ggplot')
+
+df = quandl.get('WIKI/GOOGL')
+
+df = df[['Adj. Open','Adj. Close','Adj. High','Adj. Low','Adj. Volume']]
+
+df['hl_pct'] = (df['Adj. High'] - df['Adj. Close']) / df['Adj. Close'] * 100.0
+df['pct_chg'] = (df['Adj. Close'] - df['Adj. Open']) / df['Adj. Open'] * 100.0
+
+df = df[['Adj. Close','hl_pct','pct_chg','Adj. Volume']]
+
+forecast_col = 'Adj. Close'
+df.fillna(-99999, inplace=True)
+
+forecast_out = int(math.ceil(0.01*len(df)))
+print(forecast_out)
+
+df['label'] = df[forecast_col].shift(-forecast_out)
+
+
+
+#X is feature
+#y is label
+
+X = np.array(df.drop(['label'],1))
+#X = preprocessing.scale(X)
+X_lately = X[-forecast_out:]
+X = X[:-forecast_out:]
+
+df.dropna(inplace=True)
+y = np.array(df['label'])
+y = np.array(df['label'])
+
+X_train, X_test, y_train, y_test = cross_validation.train_test_split(X, y, test_size=0.2)
+
+clf = LinearRegression()
+clf.fit(X_train, y_train)
+accuracy = clf.score(X_test, y_test)
+
+forecast_set = clf.predict(X_lately)
+
+print(forecast_set, accuracy, forecast_out)
+
+df['Forecast'] = np.nan
+
+last_date = df.iloc[-1].name
+last_unix = last_date.timestamp()
+one_day = 86400
+next_unix = last_unix + one_day
+
+for i in forecast_set:
+    next_date = datetime.datetime.fromtimestamp(next_unix)
+    next_unix += one_day
+    df.loc[next_date] = [np.nan for _ in range(len(df.columns)-1)] + [i]
+
+df['Adj. Close'].plot()
+df['Forecast'].plot()
+plt.legend(loc=4)
+plt.xlabel('Date')
+plt.ylabel('Price')
+plt.show()
