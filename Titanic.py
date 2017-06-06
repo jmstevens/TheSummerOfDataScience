8cba2a547708a9f702a1507c080fe7ce46af1b21
Joel Stevens <joel.stevens>
2017-06-06 11:28:54 -0500

    blah

--- Titanic.py
+++ Titanic.py
@@ -2,5 +2,8 @@ import pandas as pd
 
 # Read the data
 train = pd.read_csv('~/Titanic/Data/train.csv')
+test = pd.read_csv('~/Titanic/Data/test.csv')
+gender = pd.read_csv('~/Titanic/Data/gender_submission.csv')
+
 print('training set is loaded')
 print(train)
